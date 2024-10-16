22ace54c4ea459d6d56b7b4aa2bcb78a379d85fc
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-16 16:26:02 +0000

    Q2 Finished

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,22 @@
+#include "question2.h"
+#include <iostream>
+using std::cout; using std::cin;
 
 int main()
 {
+    int done,num1,num2,gcd;
+    do
+    {
+        cout<<"Enter the values you want the gcd of,\nValue 1: ";
+        cin>>num1;
+        cout<<"Value 2: ";
+        cin>>num2;
+        gcd = get_gcd(num1,num2);
+        cout<<"The GCD of "<<num1<<" and "<<num2<<" is "<<gcd<<".";
+        cout<<"\nInput 1 to terminate program, 2 to continue: ";
+        cin>>done;
+    } while (done!=1);
+    
 
     return 0;
 }
