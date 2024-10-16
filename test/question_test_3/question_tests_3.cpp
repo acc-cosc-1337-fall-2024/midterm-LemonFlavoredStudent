1f4f8e7b80ed97bf8c5921ca8a7050344ef55f02
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-16 18:43:30 +0000

    Q3 All Tests Passed

--- test/question_test_3/question_tests_3.cpp
+++ test/question_test_3/question_tests_3.cpp
@@ -10,3 +10,18 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+TEST_CASE("Verify get_cookie_ingredients")
+{
+	vector<double> ingredients= get_cookie_ingredients(96);
+	REQUIRE(ingredients[0]==3);
+	REQUIRE(ingredients[1]==2);
+	REQUIRE(ingredients[2]==5.5);
+	ingredients= get_cookie_ingredients(48);
+	REQUIRE(ingredients[0]==1.5);
+	REQUIRE(ingredients[1]==1);
+	REQUIRE(ingredients[2]==2.75);
+	ingredients= get_cookie_ingredients(24);
+	REQUIRE(ingredients[0]==.75);
+	REQUIRE(ingredients[1]==.5);
+	REQUIRE(ingredients[2]==1.375);
+}
