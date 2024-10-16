a5b492aa306e4c3ad393d8ea9bee5499ba9ea522
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-16 15:18:17 +0000

    Question 1 Main

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,21 @@
-
+#include "question1.h"
+#include <iostream>
+using std::cout; using std::cin;
 int main()
 {
+    string done;
+    int num;
+    
+    do 
+    {
+        cout<<"Input a value between 1-10 to get it's factorial.";
+        cin>>num;
+        string factorialOutput = get_factorial_sequence(num);
+        cout<<"The factorial results in: " << factorialOutput<<"\n";
+        cout<<"Would you like to stop the program? (Y/y to exit)";
+        cin>>done;
+
+    }while (done!="y" or done!="Y");
 
     return 0;
 }
