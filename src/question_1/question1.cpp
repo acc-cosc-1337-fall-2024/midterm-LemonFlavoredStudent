7866ac1e0e62066fd80a4ab2c1476d9a7b7bf4a1
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-16 14:01:36 +0000

    WIP Q1 Changes

--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -1,6 +1,24 @@
 #include "question1.h"
-
+#include <sstream>
+using std::ostringstream; using std::istringstream;
 bool test_config()
 {
     return true;
+}
+
+string get_factorial_sequence(int num)
+{
+    string factorialString = "";
+    int factorial = 1;
+
+    if (num >= 1 and num <= 10)
+    {
+        ostringstream str1;
+        for (int i = 1; i <= num; ++i)
+        {
+            factorial *= i;
+            str1 << num;
+            factorialString += str1.str();
+        }
+    }
 }
--- src/question_1/question1.h
+++ src/question_1/question1.h
@@ -1,1 +1,4 @@
+#include <string>
+using::std::string;
 bool test_config();
+string get_factorial_sequence(int num);
