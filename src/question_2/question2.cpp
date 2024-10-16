f47ac5a06dbb6a132420991a7a963edc89cee23e
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-16 15:59:12 +0000

    Q2 Changes

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -4,3 +4,22 @@ bool test_config()
 {
     return true;
 }
+
+int get_gcd(int num1, int num2)
+{
+    auto temp = 0;
+    while (num1 != num2)
+    {
+        if (num1<num2)
+        {
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        else if (num1>num2)
+        {
+            num1=num1-num2;
+        }
+    }
+    return num1;
+}
--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,2 @@
 bool test_config();
+int get_gcd(int num, int num2);
