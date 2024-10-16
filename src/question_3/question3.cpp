6ba8504de6abacad99831c62e7ee2a8edcde1863
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-16 18:27:31 +0000

    Q3 Function and Header

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -4,3 +4,13 @@ bool test_config()
 {
     return true;
 }
+
+vector <double> get_cookie_ingredients(int cookies)
+{
+    double sugar = 1.5*(cookies/48.0);
+    double butter = 1*(cookies/48.0);
+    double flour = 2.75*(cookies/48.0);
+    vector <double> ingredients;
+    ingredients.push_back(sugar); ingredients.push_back(butter); ingredients.push_back(flour);
+    return ingredients;
+}
--- src/question_3/question3.h
+++ src/question_3/question3.h
@@ -1,1 +1,4 @@
+#include <vector>
 bool test_config();
+using std::vector;
+vector <double> get_cookie_ingredients(int cookies);
