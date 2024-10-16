c99764d2d40408e53da645486c0720c31e01d387
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-10-16 19:47:36 +0000

    Q4 Finished

--- src/question_4/main.cpp
+++ src/question_4/main.cpp
@@ -1,6 +1,21 @@
-
+#include "question4.h"
+#include <iostream>
+using std::cout; using std::cin;
 int main()
-{
+{  
+    string word;
+    int done;
+    bool palindrome;
 
+    do
+    {
+        cout<<"Input the word you want to check is a palindrome (case sensitive): ";
+        cin>>word;
+        palindrome = is_palindrome(word);
+        cout<<std::boolalpha<<word<<" is palindrome = "<<palindrome;
+        cout<<"\nInput 1 to terminate program, 2 to continue: ";
+        cin>>done;
+    } while (done!=1);
+    
     return 0;
 }
